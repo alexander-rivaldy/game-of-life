f5cc0f98aea427e168a1c91165e5a8da0988a630
alexander-rivaldy <alexanderrivaldy@gmail.com>
2020-05-07 00:22:09 +1000

    first attempt to apply simulation

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,121 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import './App.css';
 import produce from 'immer';
 
-const NUM_ROWS = 50;
-const NUM_COLS = 50;
+const NUM_ROWS = 5;
+const NUM_COLS = 5;
 
-type Grid = boolean[][];
+const OPERATIONS = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+];
+
+type Grid = number[][];
 
 const App: React.FC = () => {
   const [grid, setGrid]: [Grid, (grid: Grid) => void] = useState(() => {
     const rows = [];
     for (let i = 0; i < NUM_ROWS; i++) {
-      rows.push(Array.from(Array(NUM_COLS), () => false));
+      rows.push(Array.from(Array(NUM_COLS), () => 0));
     }
     return rows;
   });
 
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const isNeighborWithinBound = (row: number, col: number): boolean =>
+    row >= 0 && row < NUM_ROWS && col >= 0 && col < NUM_COLS;
+
+  const runSimulation = () => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    console.log('old grid');
+    console.log(grid);
+
+    const newGrid = produce(grid, (gridCopy: Grid) => {
+      for (let row = 0; row < NUM_ROWS; row++) {
+        for (let col = 0; col < NUM_COLS; col++) {
+          let neighbors = 0;
+          OPERATIONS.forEach(([x, y]) => {
+            const newRow = row + x;
+            const newCol = col + y;
+
+            if (isNeighborWithinBound(newRow, newCol)) {
+              neighbors += grid[newRow][newCol];
+            }
+          });
+
+          if (neighbors < 2 || neighbors > 3) {
+            gridCopy[row][col] = 0;
+          } else if (grid[row][col] === 0 && neighbors === 3) {
+            console.log('ALIVE in ', row, col);
+            gridCopy[row][col] = 1;
+          }
+        }
+      }
+    });
+
+    console.log('updated grid');
+    console.log(newGrid);
+    setGrid(newGrid);
+    console.log('after save grid');
+    console.log(grid);
+
+    setTimeout(runSimulation, 1000);
+  };
+
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: `repeat(${NUM_COLS}, 20px)`,
-      }}
-    >
-      {grid.map((rows, rowIndex) =>
-        rows.map((col, colIndex) => (
-          <div
-            key={`box-${rowIndex}-${colIndex}`}
-            style={{
-              width: 20,
-              height: 20,
-              backgroundColor: grid[rowIndex][colIndex] ? 'pink' : undefined,
-              border: '1px solid black',
-            }}
-            onClick={() => {
-              const newGrid = produce(grid, (gridCopy) => {
-                gridCopy[rowIndex][colIndex] = !gridCopy[rowIndex][colIndex];
-              });
-              setGrid(newGrid);
-            }}
-          ></div>
-        ))
-      )}
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setRunning(!running);
+          runningRef.current = true;
+          runSimulation();
+        }}
+      >
+        {running ? 'stop' : 'start'}
+      </button>
+
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${NUM_COLS}, 20px)`,
+        }}
+      >
+        {grid.map((rows, rowIndex) =>
+          rows.map((col, colIndex) => (
+            <div
+              key={`box-${rowIndex}-${colIndex}`}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: col ? 'pink' : undefined,
+                border: '1px solid black',
+              }}
+              onClick={() => {
+                const newGrid = produce(grid, (gridCopy) => {
+                  gridCopy[rowIndex][colIndex] = col ? 0 : 1;
+                });
+                setGrid(newGrid);
+                console.log('update');
+                console.log(grid);
+              }}
+            ></div>
+          ))
+        )}
+      </div>
+    </>
   );
 };
 
